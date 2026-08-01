a43324c95e5bc4515a2c5c6de950001e70b60feb
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-07-31 22:23:45 -0400

    Initialize firestore.js with server structure

---
+++ firestore.js
@@ -0,0 +1,17 @@
+print("Hello Sharks")
+servers/
+   serverId/
+      name: "Shark Reef"
+      owner: uid
+      createdAt: timestamp
+
+servers/serverId/channels/
+   channelId/
+      name: "general"
+      createdAt: timestamp
+
+servers/serverId/channels/channelId/messages/
+   messageId/
+      author: uid
+      content: "hello ocean"
+      timestamp: timestamp
