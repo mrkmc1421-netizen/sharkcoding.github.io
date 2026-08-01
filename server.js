372ca53a5b7a9a0a14bda15c8a5c72469566d800
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-07-31 22:26:51 -0400

    Add createChannel and sendMessage functions

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ function createServer(name) {
       createdAt: firebase.firestore.FieldValue.serverTimestamp()
     });
 }
+function createChannel(serverId, name) {
+  return firebase.firestore()
+    .collection("servers")
+    .doc(serverId)
+    .collection("channels")
+    .add({
+      name,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+    }function sendMessage(serverId, channelId, content) {
+  const uid = firebase.auth().currentUser.uid;
+
+  return firebase.firestore()
+    .collection("servers")
+    .doc(serverId)
+    .collection("channels")
+    .doc(channelId)
+    .collection("messages")
+    .add({
+      author: uid,
+      content,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
+    }
